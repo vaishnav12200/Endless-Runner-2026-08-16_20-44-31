f231d707df0cfd178150803153c85d85f36e1111
vaishnav12200 <vaishnavkm2005@gmail.com>
2026-08-17 15:29:31 +0530

    Add new shaders and player movement script
    - Created TMP_SDF.shader and TMP_Sprite.shader for TextMesh Pro support.
    - Added TMP_Sprite.shader.meta and TMP_SDF.shader.meta for shader metadata.
    - Introduced TMPro.cginc, TMPro_Mobile.cginc, and TMPro_Properties.cginc for shader properties and functionality.
    - Implemented TMP_Surface.cginc for surface shading calculations.
    - Added PlayerMove.cs script for player movement logic, allowing left and right track movement.
    - Included metadata files for new scripts and shaders.

---
+++ Assets/scripts/PlayerMove.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] float moveSpeed =2;
+    [SerializeField] float xPos;
+    [SerializeField] float zPos;
+    [SerializeField] int trackNumber=1;
+    [SerializeField] int sideSpeed=9;
+    [SerializeField] bool currentMove;
+    [SerializeField] int moveDirection; // 1=left ,2 = right
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed, Space.World);
+        xPos =gameObject.transform.position.x;
+        zPos = gameObject.transform.position.z;
+        if (currentMove == true && moveDirection == 1) //movng left
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * sideSpeed, Space.World);
+            if (xPos <=trackNumber)
+            {
+                currentMove = false;
+                moveDirection = 0;
+                transform.position = new Vector3(trackNumber, 1, zPos);
+            }
+        }
+        if (currentMove == true && moveDirection == 2) //movng right
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * sideSpeed * -1, Space.World);
+            if (xPos >=trackNumber)
+            {
+                currentMove = false;
+                moveDirection = 0;
+                transform.position = new Vector3(trackNumber, 1, zPos);
+            }
+        }
+
+    }
+
+    public void LeftMove()
+    {
+        if (trackNumber == 1)
+        {
+            currentMove = true;
+            moveDirection = 1;
+            trackNumber=0;
+        }
+        if (trackNumber == 2)
+        {
+            currentMove = true;
+            moveDirection = 1;
+            trackNumber=1;
+        }
+    }
+    public void RightMove()
+    {
+        if (trackNumber == 1)
+        {
+            currentMove = true;
+            moveDirection = 2;
+            trackNumber=2;
+        }
+        if (trackNumber == 0)
+        {
+            currentMove = true;
+            moveDirection = 2;
+            trackNumber=1;
+        }
+    }
+}
+                                                                
