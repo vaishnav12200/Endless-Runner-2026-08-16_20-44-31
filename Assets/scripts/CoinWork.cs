8b5ec3192652b96e4d05cb7f051d93baded64aa3
vaishnav12200 <vaishnavkm2005@gmail.com>
2026-08-20 18:19:44 +0530

    Add coin animation and material assets
    - Created a new folder for animations and added Coin.controller and Shrink.anim files to manage coin animations.
    - Added Coin material with appropriate properties for rendering.
    - Implemented CoinWork script to handle coin behavior, including rotation, collection, and animation triggering.
    - Added metadata files for all new assets to ensure proper integration with Unity.

---
+++ Assets/scripts/CoinWork.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+
+public class CoinWork : MonoBehaviour
+{
+    [SerializeField] bool collectedCoin;
+        // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 2, 0,Space.World);
+        if (collectedCoin == true)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * 6, Space.World);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+   
+        {
+            collectedCoin = true;
+            this.gameObject.GetComponent<Animator>().Play("Shrink"); 
+            StartCoroutine(DeleteCoin());
+        }
+        IEnumerator DeleteCoin()
+        {
+            yield return new WaitForSeconds(0.5f);
+            this.gameObject.SetActive(false);
+        }
+    
+}
